1c5e929cd2c8c79c65071bccbe8dbd1b9be54bf8
snhebrok-eval VM <sven.hebrok@upb.de>
2024-03-01 13:50:40 +0100

    updated neo4j workflow

--- neo4j/import_csv.sh
+++ neo4j/import_csv.sh
@@ -1,8 +1,14 @@
-# beware: /import is not the folder in this directory, but the mountpoint within the container, which is mounted in run_neo4j.sh
-docker exec --interactive --tty steckruebe-graph-database neo4j-admin database import full \
+docker run \
+	-it \
+	--rm \
+	--name steckruebe-import \
+	-v /data/cdn_ticket/neo4j/neo4jdata:/data \
+	-v /data/cdn_ticket/neo4j/import:/import \
+	neo4j:5.13.0 \
+neo4j-admin database import full \
 	--nodes /import/domains_header.csv,/import/domains.csv \
 	--nodes /import/ips_header.csv,/import/ips.csv \
 	--nodes /import/prefixes_header.csv,/import/prefixes.csv \
 	--relationships /import/relationships_header.csv,/import/relationships.csv \
-	--overwrite-destination \
+	--overwrite-destination=true \
 	neo4j
--- neo4j/run_neo4j.sh
+++ neo4j/run_neo4j.sh
@@ -5,10 +5,10 @@ docker run \
 	-it \
 	--name steckruebe-graph-database \
 	-v /data/cdn_ticket/neo4j/neo4jdata:/data \
-	-v /data/cdn_ticket/neo4j/import:/import \
 	--env NEO4J_PLUGINS='["graph-data-science"]' \
 	--env NEO4J_AUTH='neo4j/IJj5fyYpeeWdvAXsxwuJuqlGQxZNDhLf' \
 	-p 7474:7474 \
 	-p 7687:7687 \
 	-d \
+	--rm \
 	neo4j:5.13.0
