b92aacb9e098d14ff475ef25e8fbc638b04f647d
Steckruebe <sven.hebrok@upb.de>
2024-08-01 09:25:50 +0200

    Use venv and add more logging

--- 0_all_in_one.sh
+++ 0_all_in_one.sh
@@ -7,18 +7,36 @@ if [ -d "out" ]; then
     exit 1
 fi
 
+if ! [ -d ".venv" ]; then
+    echo "[#] Creating venv"
+    python3 -m venv .venv
+    . .venv/bin/activate
+    pip install -r requirements.txt
+else
+    echo "[#] Using existing venv"
+    . .venv/bin/activate
+fi
+
 
 echo "[#] Gathering Tickets"
 
-python3 1_gather_tickets.py tranco_LJ7W4.csv 100000
+date
+python3 1_gather_tickets.py ../tranco_G6KVK.csv 1000000
+date
 
 cat out/7_merged_zgrab.r*.json > out/7_merged_zgrab_all.json
 
 cd neo4j
 docker stop steckruebe-graph-database || true
 
+if ! [ -d "import" ]; then
+    mkdir import
+fi
+
 echo "[#] Preparing for Neo4j"
+date
 python3 generate_bulk_csv.py
+date
 echo "[ ] Importing into Neo4j"
 ./import_csv.sh
 echo "[ ] Starting Neo4j"
@@ -29,7 +47,11 @@ echo "[ ] Generating Clusters"
 cd ..
 
 echo "[#] Running Scan (Sampling Neo4j)"
+date
 python3 2_perform_redirection.py
+date
 
 # echo "Running Evaluation"
+# date
 # python3 3_evaluate.py
+# date
