64220ea4851acaf2ae6ee9aa245385eee3e4f802
root <root@snhebrok-eval.cs.uni-paderborn.de>
2024-12-12 14:25:14 +0100

    Add confidence

--- 6_confidence.py
+++ 6_confidence.py
@@ -15,6 +15,8 @@ from multiprocessing.pool import ThreadPool
 from pprint import pformat, pprint
 from typing import Optional, Union
 from urllib.parse import urlparse
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
 
 import bson
 import Levenshtein
@@ -38,7 +40,10 @@ from tqdm import tqdm
 import numpy as np
 import scipy.stats as stats
 import matplotlib.pyplot as plt
+from joblib import Parallel, delayed
+import math
 
+from collections import defaultdict
 
 warnings.filterwarnings("ignore", category=XMLParsedAsHTMLWarning)
 warnings.filterwarnings("ignore", category=MarkupResemblesLocatorWarning)
@@ -55,111 +60,119 @@ class ScanContext:
     def initialize(mongo_collection_name=None, *, verify_connectivity=True):
         ScanContext.neo4j = connect_neo4j(verify_connectivity=verify_connectivity)
 
-        mongodb = connect_mongo(verify_connectivity=verify_connectivity)
-        database = mongodb["steckruebe"]
-        if not mongo_collection_name:
-            mongo_collection_name = get_most_recent_collection_name(database, "ticket_redirection_")
-            logging.info(f"Using most recent collection: {mongo_collection_name}")
-        if not mongo_collection_name:
-            raise ValueError("Could not determine most recent collection")
-        ScanContext.mongo_collection = database[mongo_collection_name]
+        #mongodb = connect_mongo(verify_connectivity=verify_connectivity)
+        #database = mongodb["steckruebe"]
+        #if not mongo_collection_name:
+        #    mongo_collection_name = get_most_recent_collection_name(database, "ticket_redirection_")
+        #    logging.info(f"Using most recent collection: {mongo_collection_name}")
+        #if not mongo_collection_name:
+        #    raise ValueError("Could not determine most recent collection")
+        #ScanContext.mongo_collection = database[mongo_collection_name]
 
 
-def calculate_confidence(same_site_similarities, cross_site_similarities, resumption_similarity, debug=False):
-    mean_x = np.mean(same_site_similarities)
-    mean_y = np.mean(cross_site_similarities)
-    if debug:
-        print("Mean X", mean_x, "Mean Y", mean_y)
+def write_transaction(tx, rel_id, property_name, property_value):
+    query = """
+    MATCH ()-[r]->()
+    WHERE elementId(r) = $rel_id
+    SET r[$property_name] = $property_value
+    """
+    result = tx.run(query, rel_id=rel_id, property_name=property_name, property_value=json.dumps(property_value))
+    return result.single()
 
-    error_x = stats.sem(same_site_similarities)
-    error_y = stats.sem(cross_site_similarities)
-    if error_x == 0.0:
-        error_x = 0.000000000001
-    if error_y == 0.0:
-        error_y = 0.000000000001
 
-    z_value_x = abs((resumption_similarity - mean_x) /error_x)
-    z_value_y = abs((resumption_similarity - mean_y) /error_y)
+def calculate_confidence(r, target_sim):
+    target_relationship = r.get("IR")
+    resumtionsimilarity = target_relationship.get(target_sim)
+    if resumtionsimilarity is None: return
+    samesitesimilarities = np.array(r.get("other_a"))
 
-    if debug:
-        print((1-stats.norm.cdf(z_value_x))*2, (1-stats.norm.cdf(z_value_y))*2)
+    mean_x = np.mean(samesitesimilarities)
+    error_x = stats.sem(samesitesimilarities, nan_policy='propagate') or 1e-12
+    z_value_x = abs((resumtionsimilarity - mean_x) /error_x)
+    cdf_x = stats.norm.sf(z_value_x)
+
+    def __do(y):
+        n = len(y)
+        if n < 2:
+            return 0.0
+        mean_y = np.mean(y)
+        #error_y_1 = stats.sem(y, nan_policy='propagate') or 1e-12
+        #Mathematical hack so things go wooosh
+        error_y = (np.std(y, ddof=1, mean=mean_y) / np.sqrt(n-1)) or 1e-12
+        z_value_y = abs((resumtionsimilarity - mean_y) /error_y)
+        cdf_y = stats.norm.sf(z_value_y)
+        return (cdf_y - cdf_x) * 2
+
 
     #-1 indicates strong confidence that we have a.com, +1 indicates strong confidence that we resumed b.com, 0 means we don't know cause it either doesn't match any or both
-    confidence = (1-stats.norm.cdf(z_value_y))*2-(1-stats.norm.cdf(z_value_x))*2
-    return confidence
+    #ab = itertools.groupby(r.get("a_b"), lambda x : x[0])
+    abc = defaultdict(list)
+    for x in r.get("a_b"): abc[x[0]].append(x[1])
+
+    confidence_per_domain = [(k, __do(np.array(v))) for k,v in abc.items()]
+    confidence_per_domain.sort(key=lambda x:x[1], reverse=True)
+    try:
+        writeresult = ScanContext.neo4j.session().execute_write(
+            write_transaction,
+            rel_id=r.get("id"),
+            property_name=f"{target_sim}_conf",
+            property_value=confidence_per_domain)  
+    except Exception as e:
+        print("Ex", e, type(e))
+    
 
 def main(collection_name=None):
-    global proc_pool
     target_sim = "similarity_levenshtein"
+    start = time.time()
 
     ScanContext.initialize(mongo_collection_name=collection_name)
     start = time.time()
     res = ScanContext.neo4j.session().run("""
-            MATCH (I)-[IR: SIM { first_color: "WHITE" }]-(R:REDIRECT_HTML)
-            WHERE IR[$sim_typ]<2
-            WITH I, IR, R,
+            MATCH (I)-[IR: SIM { first_color: "WHITE" }]-()
+            WITH I, IR,
                 COLLECT { 
                     MATCH (a)-[A:SIM]-(I)
                     WHERE a.domain = I.domain
                     AND A[$sim_typ] IS NOT NULL
-                    RETURN[a, A]
-                    ORDER BY A[$sim_typ] DESC
+                    RETURN A[$sim_typ]
                 } as other_a,
                 COLLECT {
                     MATCH (I)-[B: SIM]-(b)
                     WHERE B[$sim_typ] IS NOT NULL
                     AND b.domain <> I.domain
-                    RETURN[b, B]
-                    ORDER BY B[$sim_typ] DESC
+                    RETURN [b.domain, B[$sim_typ]]
                 } as a_b
-            RETURN I, IR, R, other_a, a_b
-            LIMIT 10000""", { "sim_typ":  target_sim}, routing_=RoutingControl.READ)
-    print("Took ", time.time()-start)
-    max_confidence_value_pairs = {}
-    for r in res:
-        #print(r.get("I"))
-        #print(r.get("IR"))
-        #print(r.get("R"))
-        #print()
-        #for x in r.get("other_a"):
-        #    print(x[0].get("domain"), x[1].get(target_sim))
-        samesitesimilarities = [x[1].get(target_sim) for x in r.get("other_a")]
-        #print()
-        othersitesimilaritiesdict  = {}
-        for x in r.get("a_b"):
-            domain = x[0].get("domain")
-            if domain not in othersitesimilaritiesdict:
-                othersitesimilaritiesdict[domain] = []
-            othersitesimilaritiesdict[domain].append(x[1].get(target_sim))
-
-        #    print(x[0].get("domain"), x[1].get(target_sim))
-        #othersitesimilarities = [x[1].get(target_sim) for x in r.get("a_b")]
-        resumtionsimilarity = r.get("IR").get(target_sim)
-        #print(samesitesimilarities)
-        #print(othersitesimilarities)
-        #print(resumtionsimilarity)
-        confidence_values = []
-        for k,v in othersitesimilaritiesdict.items():
-            if resumtionsimilarity is None: continue
-            confidence = calculate_confidence(np.array(samesitesimilarities), np.array(v), resumtionsimilarity, debug=False)
-            confidence_values.append((confidence, k))
-        if len(confidence_values) > 0:
-            max_confidence = max(confidence_values, key=lambda k : k[0])
-            if max_confidence[0]>0.9:        
-                print("Domain", r.get("I").get("domain"), "most likely resumes to", max_confidence[1] ,"with confidence", max_confidence[0])
-                print("\n\n")
-            max_confidence_value_pairs[max_confidence[1]] = max_confidence[0]
+            RETURN elementId(IR) as id, IR, other_a, a_b
+            LIMIT 1000
+            """, { "sim_typ":  target_sim}, routing_=RoutingControl.READ)
+#                    ORDER BY b.domain
+    with ProcessPoolExecutor() as executor:
+        executor.map(lambda r: calculate_confidence(r, target_sim), res)
+    #for r in res:
+    #    calculate_confidence(r, target_sim)
+    print(time.time() - start)
     
-    plt.hist(list(max_confidence_value_pairs.values()), bins=200)
-    plt.grid(True, linestyle='--', alpha=0.5)
-    plt.yscale('log')
-    plt.savefig('hist.png')
+    #plt.hist(list(max_confidence_value_pairs.values()), bins=200)
+    #plt.grid(True, linestyle='--', alpha=0.5)
+    #plt.yscale('log')
+    #plt.savefig('hist.png')
+
 
 def test(collection_name=None):
     global proc_pool
     target_sim = "similarity_levenshtein"
 
     ScanContext.initialize(mongo_collection_name=collection_name)
+    result = ScanContext.neo4j.session().run(
+        """
+        MATCH (I)-[IR:SIM { first_color: "WHITE" }]-(R:REDIRECT_HTML)
+        RETURN COUNT(*) AS result_count
+        """
+    )
+    print(result.single()["result_count"])
+
+    return
+
     start = time.time()
     res = ScanContext.neo4j.session().run("""
             MATCH (I)-[IR: SIM { first_color: "WHITE" }]-(R:REDIRECT_HTML)
@@ -183,6 +196,7 @@ def test(collection_name=None):
     print(i)
 
 
+
 if __name__ == "__main__":
     logging.basicConfig(
         level=logging.INFO,
@@ -192,7 +206,7 @@ if __name__ == "__main__":
     logging.getLogger("neo4j").setLevel(logging.WARNING)
     logging.getLogger("pymongo").setLevel(logging.WARNING)
     try:
-        test("ticket_redirection_2024-08-19_19:28")
+        main("ticket_redirection_2024-08-19_19:28")
     except:
         LOGGER.exception("Error in main")
         raise
