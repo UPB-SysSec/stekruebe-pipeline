07d2a50fb58345a59923cd926e828437243aeec3
Steckruebe <sven.hebrok@upb.de>
2025-04-26 12:30:53 +0200

    improved sankey for stats

--- plot_stats.py
+++ plot_stats.py
@@ -9,7 +9,7 @@ import json
 import types
 import inspect
 
-OUTDIR = functools.partial(op.join, "out")
+OUTDIR = functools.partial(op.join, "out_submission")
 TRANCO = "../tranco_V9V2N.csv"
 
 _ERROR_NODES = False
@@ -75,8 +75,12 @@ class Domain:
     phase_associations: dict["Phase Class", Node] = field(default_factory=dict)
     ip4s: set[str] = field(default_factory=set)
     ip6s: set[str] = field(default_factory=set)
-    zgrab_status12: set[str] = field(default_factory=set)
-    zgrab_status13: set[str] = field(default_factory=set)
+    unblocked_4s: set[str] = field(default_factory=set)
+    unblocked_6s: set[str] = field(default_factory=set)
+    open_4s: set[str] = field(default_factory=set)
+    open_6s: set[str] = field(default_factory=set)
+    zgrab_status12: list[str] = field(default_factory=list)
+    zgrab_status13: list[str] = field(default_factory=list)
 
 
 PHASES = []
@@ -161,7 +165,7 @@ class zmap:
     closed = Node("Closed", COLORS.black, None, True)
 
 
-_SUMMARIZE_ZGRAB_ERRORS = True
+_SUMMARIZE_ZGRAB_ERRORS = False
 
 
 @phase
@@ -205,142 +209,190 @@ for resolved_file in glob.glob(OUTDIR("0_resolved*.json")):
             domain.phase_associations[inputs] = inputs.SANS
 
 
-print("Loading IP Addresses")
-for resolved_file in glob.glob(OUTDIR("0_resolved*.json")):
-    with open(resolved_file) as f:
-        for ln in f:
-            item = json.loads(ln)
-            domain_name = item.get("name")
-            if not domain_name:
-                print("No domain item in", f.name)
-                continue
-            domain = domains[domain_name]
-            data = item.get("data") or {}  # assigns dict() even when data exists but null
-            domain.ip4s.update(data.get("ipv4_addresses", []))
-            domain.ip6s.update(data.get("ipv6_addresses", []))
-            domain.phase_associations[zdns] = getattr(zdns, item["status"])
-for domain in domains.values():
-    if domain.ip4s and domain.ip6s:
-        domain.phase_associations[resolved] = resolved.v4_6
-    elif domain.ip4s:
-        domain.phase_associations[resolved] = resolved.v4
-    elif domain.ip6s:
-        domain.phase_associations[resolved] = resolved.v6
-    else:
-        domain.phase_associations[resolved] = resolved.no_ip
-
-print("Loading blocked")
-blocklist_ip4s = set()
-blocklist_ip6s = set()
-for resolved_filtered_file_v4 in glob.glob(OUTDIR("2_resolved_filtered_v4*.ips")):
-    with open(resolved_filtered_file_v4) as f4:
-        blocklist_ip4s.update(map(lambda x: x.strip(), f4))
-for resolved_filtered_file_v6 in glob.glob(OUTDIR("2_resolved_filtered_v6*.ips")):
-    with open(resolved_filtered_file_v6) as f6:
-        blocklist_ip6s.update(map(lambda x: x.strip(), f6))
-
-for domain in domains.values():
-    overlap4 = domain.ip4s & blocklist_ip4s
-    overlap6 = domain.ip6s & blocklist_ip6s
-    if overlap4 and overlap6:
-        domain.phase_associations[blocklist] = blocklist.v4_6
-    elif overlap4:
-        domain.phase_associations[blocklist] = blocklist.v4
-    elif overlap6:
-        domain.phase_associations[blocklist] = blocklist.v6
-    else:
-        domain.phase_associations[blocklist] = blocklist.blocklist
-# blocklist_ip4s.clear()
-# blocklist_ip6s.clear()
-
-print("Loading zmap results")
-zmap_ip4s = set()
-zmap_ip6s = set()
-for zmap_file_v4 in glob.glob(OUTDIR("3_https_hosts_v4*.ips")):
-    with open(zmap_file_v4) as f4:
-        zmap_ip4s.update(map(lambda x: x.strip(), f4))
-for zmap_file_v6 in glob.glob(OUTDIR("3_https_hosts_v6*.ips")):
-    with open(zmap_file_v6) as f6:
-        zmap_ip6s.update(map(lambda x: x.strip(), f6))
-
-for domain in domains.values():
-    overlap4 = domain.ip4s & zmap_ip4s & blocklist_ip4s
-    overlap6 = domain.ip6s & zmap_ip6s & blocklist_ip6s
-    if overlap4 and overlap6:
-        domain.phase_associations[zmap] = zmap.v4_6
-    elif overlap4:
-        domain.phase_associations[zmap] = zmap.v4
-    elif overlap6:
-        domain.phase_associations[zmap] = zmap.v6
-    else:
-        domain.phase_associations[zmap] = zmap.closed
-zmap_ip4s.clear()
-zmap_ip6s.clear()
-
-print("Loading zgrab")
-for zgrab_file in glob.glob(OUTDIR("7_merged_zgrab*.json")):
-    with open(zgrab_file) as f:
-        for ln in f:
-            item = json.loads(ln)
-            domain_name = item["domain"]
-            domain = domains[domain_name]
-
-            def get_status(item, ticket_key):
-                status = item["status"]
-                if ticket_key in item:
-                    assert status == "success"
-                    return "ticket"
-                else:
-                    return item["status"]
-
-            for result in item["results"]:
-                domain.zgrab_status12.add(get_status(result["https-tls1_0-1_2"], "ticket"))
-                domain.zgrab_status13.add(get_status(result["https-tls1_3"], "tickets"))
-
-for domain in domains.values():
-    # tickets = sum(map(lambda x: 1 if x.get("ticket") else 0, item["results"]))
-
-    # STATUSes per version: ticket, no_ticket, error
-    errors = set()
-
-    def flatten_statii(statii):
-        if "ticket" in statii:
-            return "ticket"
-        elif "success" in statii:
-            return "no_ticket"
+def _handle_resolved():
+    print("Loading IP Addresses")
+    for resolved_file in glob.glob(OUTDIR("0_resolved*.json")):
+        with open(resolved_file) as f:
+            for ln in f:
+                item = json.loads(ln)
+                domain_name = item.get("name")
+                if not domain_name:
+                    print("No domain item in", f.name)
+                    continue
+                domain = domains[domain_name]
+                data = item.get("data") or {}  # assigns dict() even when data exists but null
+                domain.ip4s.update(data.get("ipv4_addresses", []))
+                domain.ip6s.update(data.get("ipv6_addresses", []))
+                domain.phase_associations[zdns] = getattr(zdns, item["status"])
+    for domain in domains.values():
+        if domain.ip4s and domain.ip6s:
+            domain.phase_associations[resolved] = resolved.v4_6
+        elif domain.ip4s:
+            domain.phase_associations[resolved] = resolved.v4
+        elif domain.ip6s:
+            domain.phase_associations[resolved] = resolved.v6
         else:
-            errors.update(statii)
-            return "error"
+            domain.phase_associations[resolved] = resolved.no_ip
 
-    status_12 = flatten_statii(domain.zgrab_status12)
-    status_13 = flatten_statii(domain.zgrab_status13)
 
-    if status_12 == "ticket" and status_13 == "ticket":
-        classification = zgrab.ticket_12_13
-    elif status_12 == "ticket":
-        classification = zgrab.ticket_12
-    elif status_13 == "ticket":
-        classification = zgrab.ticket_13
-    elif status_12 == "no_ticket" or status_13 == "no_ticket":
-        classification = zgrab.no_ticket
-    else:
-        if _SUMMARIZE_ZGRAB_ERRORS:
-            classification = zgrab.error
+_handle_resolved()
+
+
+def _handle_after_blocklist() -> types.NoneType:
+    print("Loading blocked")
+    non_blocked_ip4s = set()
+    non_blocked_ip6s = set()
+    for resolved_filtered_file_v4 in glob.glob(OUTDIR("2_resolved_filtered_v4*.ips")):
+        with open(resolved_filtered_file_v4) as f4:
+            non_blocked_ip4s.update(map(lambda x: x.strip(), f4))
+    for resolved_filtered_file_v6 in glob.glob(OUTDIR("2_resolved_filtered_v6*.ips")):
+        with open(resolved_filtered_file_v6) as f6:
+            non_blocked_ip6s.update(map(lambda x: x.strip(), f6))
+
+    for domain in domains.values():
+        remaining4 = domain.ip4s & non_blocked_ip4s
+        remaining6 = domain.ip6s & non_blocked_ip6s
+        domain.unblocked_4s = remaining4
+        domain.unblocked_6s = remaining6
+        if remaining4 and remaining6:
+            domain.phase_associations[blocklist] = blocklist.v4_6
+        elif remaining4:
+            domain.phase_associations[blocklist] = blocklist.v4
+        elif remaining6:
+            domain.phase_associations[blocklist] = blocklist.v6
         else:
-            # classification = "+".join(sorted(errors))
-            if len(errors) > 1:
-                classification = zgrab.multiple_errors
+            domain.phase_associations[blocklist] = blocklist.blocklist
+    # non_blocked_ip4s.clear()
+    # non_blocked_ip6s.clear()
+
+
+_handle_after_blocklist()
+
+
+def _handle_zmap():
+    print("Loading zmap results")
+    zmap_ip4s = set()
+    zmap_ip6s = set()
+    for zmap_file_v4 in glob.glob(OUTDIR("3_https_hosts_v4*.ips")):
+        with open(zmap_file_v4) as f4:
+            zmap_ip4s.update(map(lambda x: x.strip(), f4))
+    for zmap_file_v6 in glob.glob(OUTDIR("3_https_hosts_v6*.ips")):
+        with open(zmap_file_v6) as f6:
+            zmap_ip6s.update(map(lambda x: x.strip(), f6))
+
+    for _domain_name, domain in domains.items():
+        open4 = domain.ip4s & zmap_ip4s
+        open6 = domain.ip6s & zmap_ip6s
+        domain.open_4s = open4
+        domain.open_6s = open6
+        if open4 and open6:
+            domain.phase_associations[zmap] = zmap.v4_6
+        elif open4:
+            domain.phase_associations[zmap] = zmap.v4
+        elif open6:
+            domain.phase_associations[zmap] = zmap.v6
+        else:
+            domain.phase_associations[zmap] = zmap.closed
+    # zmap_ip4s.clear()
+    # zmap_ip6s.clear()
+
+
+_handle_zmap()
+
+
+def _handle_zgrab():
+    print("Loading zgrab")
+    for zgrab_file in glob.glob(OUTDIR("7_merged_zgrab.r*.json")):
+        # for zgrab_file in glob.glob(OUTDIR("7_merged_zgrab_all.json")):
+        print(f"-{zgrab_file}")
+        with open(zgrab_file) as f:
+            for ln in f:
+                item = json.loads(ln)
+                domain_name = item["domain"]
+                domain = domains[domain_name]
+
+                def get_status(item, ticket_key):
+                    status = item["status"]
+                    if ticket_key in item:
+                        assert status == "success"
+                        return "ticket"
+                    elif status == "success" and (ticket_key not in item or not item[ticket_key]):
+                        return "no_ticket"
+                    elif status == "success":
+                        return "weirdsuccess"
+                    else:
+                        return item["status"]
+
+                for result in item["results"]:
+                    domain.zgrab_status12.append(get_status(result["https-tls1_0-1_2"], "ticket"))
+                    domain.zgrab_status13.append(get_status(result["https-tls1_3"], "tickets"))
+                assert domain.zgrab_status12
+                assert domain.zgrab_status13
+
+    print("-postprocessing zgrab")
+    for _domain_name, domain in domains.items():
+        # tickets = sum(map(lambda x: 1 if x.get("ticket") else 0, item["results"]))
+
+        # STATUSes per version: ticket, no_ticket, error
+        errors = set()
+
+        def flatten_statii(statii):
+            if not statii:
+                return "did not run"
+            if "ticket" in statii:
+                return "ticket"
+            elif "no_ticket" in statii:
+                return "no_ticket"
             else:
-                classification = errors.pop()
+                errors.update(statii)
+                return "error"
 
-    if isinstance(classification, str):
-        if not hasattr(zgrab, classification):
-            print("Adding", classification)
-            setattr(zgrab, classification, Node(classification, COLORS.red, None, True))
-        domain.phase_associations[zgrab] = getattr(zgrab, classification)
-    else:
-        domain.phase_associations[zgrab] = classification
+        status_12 = flatten_statii(domain.zgrab_status12)
+        status_13 = flatten_statii(domain.zgrab_status13)
 
+        if status_12 == "ticket" and status_13 == "ticket":
+            classification = zgrab.ticket_12_13
+        elif status_12 == "ticket":
+            classification = zgrab.ticket_12
+        elif status_13 == "ticket":
+            classification = zgrab.ticket_13
+        elif status_12 == "no_ticket" or status_13 == "no_ticket":
+            classification = zgrab.no_ticket
+        elif status_12 == "did not run" and status_13 == "did not run":
+            classification = "did_not_run_12_13"
+        elif status_12 == "did not run":
+            classification = "did_not_run_12"
+        elif status_13 == "did not run":
+            classification = "did_not_run_13"
+        else:
+            if _SUMMARIZE_ZGRAB_ERRORS:
+                classification = zgrab.error
+            else:
+                # classification = "+".join(sorted(errors))
+                if len(errors) > 1:
+                    classification = zgrab.multiple_errors
+                else:
+                    if not errors:
+                        print("No error, no ticket, no no_ticket")
+                        print(domain.zgrab_status12, domain.zgrab_status13)
+                        print(status_12, status_13)
+                        print(status_12 == "ticket", status_13 == "ticket")
+                        print(status_12 == "ticket" and status_13 == "ticket")
+                        print("ticket" in domain.zgrab_status12, "ticket" in domain.zgrab_status13)
+                        print("no_ticket" in domain.zgrab_status12, "no_ticket" in domain.zgrab_status13)
+                        errors.add("no_error")
+                    classification = errors.pop()
+
+        if isinstance(classification, str):
+            if not hasattr(zgrab, classification):
+                print("Adding", classification)
+                setattr(zgrab, classification, Node(classification, COLORS.red, None, True))
+            domain.phase_associations[zgrab] = getattr(zgrab, classification)
+        else:
+            domain.phase_associations[zgrab] = classification
+
+
+_handle_zgrab()
 
 print("Loading Nodes")
 nodes = []
@@ -445,6 +497,10 @@ fig = go.Figure(
     ]
 )
 
+fig.update_traces(texttemplate="%{value:.10f}")
+
 # fig.update_layout(title_text="Basic Sankey Diagram", font_size=10)
 # fig.show()
 fig.write_html("graph_res/stats.html")
+with open("raw_data.txt", "w") as f:
+    f.write(str(nodes))
